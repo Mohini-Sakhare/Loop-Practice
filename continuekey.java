8ae3f6d9476d507a6e3ab2ffd43e0e173a26122d
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-13 19:49:43 +0530

    continue keyword

---
+++ continuekey.java
@@ -0,0 +1,16 @@
+//display numbers except number in a condition
+
+public class continuekey
+{
+   public static void main(String args[])
+   {
+     for(int i=1; i<=5; i++)
+     {
+        if(i==3)
+        {
+            continue;
+        }
+        System.out.println(i);
+     }  
+   }    
+}
