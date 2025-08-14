e6f39afb259294d1a2389375686b1160771890af
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-14 20:48:39 +0530

    while loop example

---
+++ whileloop1.java
@@ -0,0 +1,17 @@
+// print hello world 10 times using while loop
+
+public class whileloop1 
+{
+   public static void main(String args[])
+   {
+    int counter=0;
+
+    //use while loop
+    while(counter<10)
+    {
+        System.out.println("Hello world");
+        counter++;
+    }
+    System.out.println("print hello world 10x");
+   }    
+}
