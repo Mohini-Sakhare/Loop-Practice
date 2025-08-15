09ecc0e39f17e74d37815097057b03af2fb46d5c
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-15 21:48:38 +0530

    Loop Practice 1

---
+++ loopractice1.java
@@ -0,0 +1,14 @@
+
+//How many times 'hello' is pprinted
+
+public class loopractice1
+{
+    public static void main(String args[])
+    {
+        for(int i=0; i<5; i++)
+        {
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
